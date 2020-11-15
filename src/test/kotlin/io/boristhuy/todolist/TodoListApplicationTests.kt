962964dc3791e112f395709db688b31a24246d49
Boris Thuy <boristhuy@gmail.com>
2020-11-15 16:57:07 +0100

    Initial commit

---
+++ src/test/kotlin/io/boristhuy/todolist/TodoListApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.boristhuy.todolist
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
